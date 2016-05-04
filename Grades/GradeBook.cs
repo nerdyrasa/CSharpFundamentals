93e858533290ea0f859bf108662ec71066b969c7
nerdy rasa <rozisaacson@users.noreply.github.com>
2016-05-04 11:09:57 -0500

    Added functionality to compute grade statistics.

---
+++ Grades/GradeBook.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+
+    // classes are reference types
+    // variables hold a pointer value
+    // can have multiple variables pointing to the same object
+
+    class GradeBook
+    {
+        // class members define
+        // 1. state (nouns)
+        // 2. behavior (verbs)
+
+        // ctor code snippet -- press tab twice. Easy peasy.
+        // constructor
+        public GradeBook()
+        {
+            grades = new List<float>();
+        }
+
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+
+        public GradeStatistics ComputeStatistics()
+        {
+            GradeStatistics stats = new GradeStatistics();
+
+            float sum = 0;
+
+            foreach (float grade in grades)
+            {
+                stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
+                stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
+                sum += grade;
+            }
+            stats.AverageGrade = sum / grades.Count;
+
+            return stats;
+        }
+
+        // This is a field.
+        // generic type syntax
+        // a list that can hold zero or more floating point numbers
+
+
+        private List<float> grades;
+
+        // Field initializer syntax
+        // List<float> grades = new List<float>();
+        
+    }
+}
--- Grades/Program.cs
+++ Grades/Program.cs
@@ -10,6 +10,19 @@ namespace Grades
     {
         static void Main(string[] args)
         {
+            GradeBook book = new GradeBook();
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+            book.AddGrade(75);
+
+            // Setting it up this way gives a separation between the act of
+            // computing statistics and storing the statistics
+            // You can often reduce the complexity of an application, by adding
+            // more classes. This abstracts away the complexity.
+            GradeStatistics stats = book.ComputeStatistics();
+            Console.WriteLine(stats.AverageGrade);
+            Console.WriteLine(stats.HighestGrade);
+            Console.WriteLine(stats.LowestGrade);
         }
     }
 }
